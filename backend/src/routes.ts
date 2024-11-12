aefe9b639538fe22017fdf716ad1243fe42cd060
Frederic Chien <fred_chien@yahoo.com.br>
2024-11-12 16:00:04 -0300

    feat: create category service and controller

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,20 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 
-const router = Router();;
+import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 
-router.get('/teste', (req: Request, res: Response) => {
-    return res.json({message: 'Hello World!'});
-})
+import { isAuthenticated } from './middlewares/isAuthenticated';
+
+const router = Router();
+
+router.post('/users', new CreateUserController().handle)
+
+router.post('/session', new AuthUserController().handle)
+
+router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export { router };
